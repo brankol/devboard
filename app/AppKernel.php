77899894df82fea9e91f57ce064de19c601a3c01
Miro Svrtan <miro@mirosvrtan.me>
2015-07-18 21:21:35 +0200

    Integrating NullDevBundle into app

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new NullDev\UserBundle\NullDevUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
