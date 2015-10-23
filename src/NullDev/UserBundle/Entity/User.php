312acdd4e106e26c23d3e3227e5935f1542c3014
Miro Svrtan <miro@mirosvrtan.me>
2015-10-23 23:31:49 +0200

    Upgrading FOSUserBundle

--- src/NullDev/UserBundle/Entity/User.php
+++ src/NullDev/UserBundle/Entity/User.php
@@ -2,7 +2,7 @@
 namespace NullDev\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * @ORM\Entity
