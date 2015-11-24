3b6e70c4c9756899037592516bd985c9dc44e177
Miro Svrtan <miro@mirosvrtan.me>
2015-11-25 00:33:35 +0100

    Moved dashboard into seperate controller + basic ajax refresh

---
+++ src/DevBoard/CoreBundle/Controller/DashboardController.php
@@ -0,0 +1,45 @@
+<?php
+namespace DevBoard\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class DashboardController.
+ */
+class DashboardController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        $data = [];
+
+        return $this->render('DevBoardCoreBundle:Dashboard:index.html.twig', $data);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function liveBranchesAction()
+    {
+        $em = $this->getDoctrine();
+
+        $projects = $this->getProjects();
+        $repos    = $em->getRepository('GhRepo:GithubRepo')->getRepoIdsFromProjectIds($projects);
+        $branches = $em->getRepository('GhBranch:GithubBranch')->getBranchesFromRepoIds($repos);
+
+        $data = [
+            'branches' => $branches,
+        ];
+
+        return $this->render('DevBoardCoreBundle:Dashboard:livebranches.html.twig', $data);
+    }
+
+    private function getProjects()
+    {
+        return $this->getDoctrine()
+            ->getRepository('DevBoardProject:Project')
+            ->getUserProjectIds($this->getUser());
+    }
+}
