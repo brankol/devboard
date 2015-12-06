532473a441a11a7521daa23504816439104f9b18
Miro Svrtan <miro@mirosvrtan.me>
2015-12-06 23:44:22 +0100

    Fixing up pull request notfications

--- src/DevBoard/GithubEvent/EventHandler.php
+++ src/DevBoard/GithubEvent/EventHandler.php
@@ -3,8 +3,10 @@ namespace DevBoard\GithubEvent;
 
 use DevBoard\Github\WebHook\Data\AbstractEvent;
 use DevBoard\GithubEvent\Payload\PayloadFactory;
+use DevBoard\GithubEvent\Payload\PullRequestPayload;
 use DevBoard\GithubEvent\Payload\PushPayload;
 use DevBoard\GithubEvent\Payload\StatusPayload;
+use DevBoard\GithubEvent\PullRequest\PullRequestHandler;
 use DevBoard\GithubEvent\Push\PushHandler;
 use DevBoard\GithubEvent\Status\StatusHandler;
 use Exception;
@@ -17,19 +19,26 @@ class EventHandler
     private $payloadFactory;
     private $pushHandler;
     private $statusHandler;
+    protected $pullRequestHandler;
 
     /**
      * EventHandler constructor.
      *
-     * @param PayloadFactory $payloadFactory
-     * @param PushHandler    $pushHandler
-     * @param StatusHandler  $statusHandler
+     * @param PayloadFactory     $payloadFactory
+     * @param PushHandler        $pushHandler
+     * @param StatusHandler      $statusHandler
+     * @param PullRequestHandler $pullRequestHandler
      */
-    public function __construct(PayloadFactory $payloadFactory, PushHandler $pushHandler, StatusHandler $statusHandler)
-    {
-        $this->payloadFactory = $payloadFactory;
-        $this->pushHandler    = $pushHandler;
-        $this->statusHandler  = $statusHandler;
+    public function __construct(
+        PayloadFactory $payloadFactory,
+        PushHandler $pushHandler,
+        StatusHandler $statusHandler,
+        PullRequestHandler $pullRequestHandler
+    ) {
+        $this->payloadFactory     = $payloadFactory;
+        $this->pushHandler        = $pushHandler;
+        $this->statusHandler      = $statusHandler;
+        $this->pullRequestHandler = $pullRequestHandler;
     }
 
     /**
@@ -47,6 +56,8 @@ class EventHandler
             $this->pushHandler->process($payload);
         } elseif ($payload instanceof StatusPayload) {
             $this->statusHandler->process($payload);
+        } elseif ($payload instanceof PullRequestPayload) {
+            $this->pullRequestHandler->process($payload);
         } else {
             throw new Exception('Non supported payload:'.get_class($payload));
         }
