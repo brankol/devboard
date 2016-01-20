60820392fa2801e07533350ac223eb5d3bc6f510
Miro Svrtan <miro@mirosvrtan.me>
2016-01-20 12:31:41 +0100

    Added project & gh repo fixture
    Part of #229

---
+++ src/DevBoard/CoreBundle/DataFixtures/ORM/LoadProjectFixtures.php
@@ -0,0 +1,52 @@
+<?php
+namespace DevBoard\CoreBundle\DataFixtures\ORM;
+
+use DevBoard\Core\Project\Entity\Project;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use NullDev\UserBundle\Entity\User;
+
+/**
+ * Class LoadProjectFixtures.
+ */
+class LoadProjectFixtures extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $users = [
+            $this->getReference('user-visitor1'),
+            $this->getReference('user-msvrtan'),
+            $this->getReference('user-brankol'),
+        ];
+
+        $publicRepos = [
+            $this->getReference('gh-repo-devboard/test-assassin'),
+            $this->getReference('gh-repo-devboard/test-hitman'),
+            $this->getReference('gh-repo-devboard/test-mermaid'),
+        ];
+
+        foreach ($publicRepos as $repo) {
+            foreach ($users as $user) {
+                $project = new Project();
+                $project->setProjectName($repo->getFullName())
+                    ->addGithubRepo($repo)
+                    ->setUser($user);
+
+                $manager->persist($project);
+            }
+        }
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 90;
+    }
+}
